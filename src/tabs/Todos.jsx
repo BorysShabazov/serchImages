9b2d3f106c0327f66a21f6cf0ad8a29be27ee826
Maksym Triteinikov <i.max.trit@gmail.com>
2023-08-12 17:09:00 +0300

    task2.1

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,33 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  onSubmit = text => {
+    const todo = {
+      id: nanoid(),
+      text: text,
+    };
+    this.setState(prevState => ({ todos: [...prevState.todos, todo] }));
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    const { todos } = this.state;
+    return (
+      <>
+        <SearchForm onSubmit={this.onSubmit} />
+        <Grid>
+          {todos.map((todo, idx) => {
+            return (
+              <GridItem key={todo.id}>
+                <Todo todo={todo} number={idx + 1} />
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </>
+    );
   }
 }
